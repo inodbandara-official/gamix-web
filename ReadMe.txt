44be904aab83147c9db799e10ac8cb9c49a830ba
Inod Bandara <87967504+inodbandara-official@users.noreply.github.com>
2024-02-12 11:12:41 +0530

    WebSite

---
+++ ReadMe.txt
@@ -0,0 +1,4 @@
+To run the web application fully, use the main.html file in the web folder.
+
+Developed by,
+	Master Oogway
---
+++ quiz/js/questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+    numb: 1,
+    question: "The best-selling video game to date is,",
+    answer: "Minecraft",
+    options: [
+      "PUBG",
+      "Minecraft",
+      "Free Fire",
+      "Call of Duty Mobile"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the most downloaded mobile game in the world?",
+    answer: "Mobile Legends: Bang Bang",
+    options: [
+      "Mobile Legends: Bang Bang",
+      "Clash of Clans",
+      "Candy Crush Saga",
+      "Pokemon Go"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the most played video game by player count?",
+    answer: "PUBG Mobile",
+    options: [
+      "Battlegrounds Mobile",
+      "Peacekeeper Elite",
+      "PUBG Battlegrounds",
+      "PUBG Mobile"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the most popular racing game?",
+    answer: "GTA V",
+    options: [
+      "GTA V",
+      "Forza Horizon 5",
+      "Crab Game",
+      "Rocket League"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is the no 1 racing game in the world?",
+    answer: "Mario Kart",
+    options: [
+      "NFS Most Wanted",
+      "Forza Horizon 5",
+      "assetto corsa",
+      "Mario Kart"
+    ]
+  },
+    {
+    numb: 6,
+    question: "What is the game with the most awards?",
+    answer: "Last of Us - Part 2",
+    options: [
+      "Last of Us - Part 2",
+      "Call of Duty Mobile",
+      "PUBG Battlegrounds",
+      "Forza Horizon 4"
+    ]
+  },
+    {
+    numb: 7,
+    question: "Which gaming console came first??",
+    answer: "Xbox 360",
+    options: [
+      "PS4",
+      "Nintendo Wii",
+      "PS3",
+      "Xbox 360"
+    ]
+  },
+    {
+    numb: 8,
+    question: "COD 2 is set during a war, which war is it?",
+    answer: "World War 2",
+    options: [
+      "World War 1",
+      "World War 2",
+      "Boston Tea Party",
+      "Pearl Harbor Attack"
+    ]
+  },
+    {
+    numb: 9,
+    question: "What is the most viewed video game trailer?",
+    answer: "Star Wars Jedi",
+    options: [
+      "Armoured Core VI",
+      "Suicide Squad",
+      "Star Wars Jedi",
+      "Cyberpunk 2077"
+    ]
+  },
+    {
+    numb: 10,
+    question: "Which game made the most money in 2022?",
+    answer: "Honor of kings",
+    options: [
+      "Marvel Snap",
+      "Honor of kings",
+      "Stray",
+      "Splatoon 3"
+    ]
+  }
+];
